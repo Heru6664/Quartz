e242168d6e94e30252b08a94e91b9ef50f72b346
Heru6664 <herujulyanto999@gmail.com>
2018-05-31 14:13:29 +0700

    Authentication Login

--- src/index.js
+++ src/index.js
@@ -4,33 +4,43 @@
  * @flow
  */
 
-import React, { Component } from 'react';
-import {StyleSheet, View, Text} from 'react-native';
-import Home from './screens/Home';
+import React, { Component } from "react";
+import { StyleSheet, View, Text } from "react-native";
+import Home from "./screens/Home";
+import { createStackNavigator } from "react-navigation";
+import Dashboard from "./screens/Dashboard";
 
-export default class App extends Component{
+const Root = createStackNavigator(
+  {
+    Home: Home,
+    Dashboard: Dashboard
+  },
+  {
+    headerMode: "none"
+  }
+);
+
+export default class App extends Component {
   render() {
-    return (
-      <Home style={styles.container }/>
-    );
+    return <Root />;
   }
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#F5FCFF"
   },
   welcome: {
     fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
+    textAlign: "center",
+    margin: 10
   },
   instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
+    textAlign: "center",
+    color: "#333333",
+    marginBottom: 5
+  }
 });
--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,68 +1,99 @@
-import React, {Component} from 'react';
-import {Text, StyleSheet, Alert } from 'react-native';
-import { View, Container, Header, Content, Item, Input, Title, Left, Button} from 'native-base';
+import React, { Component } from "react";
+import { Text, StyleSheet, Alert } from "react-native";
+import {
+  View,
+  Container,
+  Header,
+  Content,
+  Item,
+  Input,
+  Title,
+  Left,
+  Button,
+  Toast
+} from "native-base";
 
-export default class Home extends Component{
+export default class Home extends Component {
+  state = {
+    username: "",
+    password: ""
+  };
 
-    render(){
-        return (
-            <Container>
-                <Header style={styles.header} span>
-                    <Title> Quartz PE </Title>
-                </Header>
-                <Content style={styles.content}>
-                    <Item style={styles.item} rounded>
-                        <Input placeholder="Username" />                        
-                    </Item>
-                    <Item style={styles.item} rounded>
-                        <Input placeholder="Password" secureTextEntry/>                        
-                    </Item>
-                    
-                    <Button style={styles.button} rounded> 
-                        <Text style={styles.text}>LOGIN</Text>
-                    </Button>
-                    <Button style={styles.button2} bordered> 
-                        <Text style={styles.text2}>REGISTER</Text>
-                    </Button>
-                </Content>
-            </Container>
-        );
+  _handleLogin = () => {
+    if (this.state.username === "heru" && this.state.password === "heruheru") {
+      this.props.navigation.navigate("Dashboard");
     }
+    if (this.state.username === "" && this.state.password === "") {
+      Alert.alert("Please enter username and password");
+    } else {
+      Alert.alert("Wrong username or password!");
+    }
+  };
+  render() {
+    return (
+      <Container>
+        <Header style={styles.header} span>
+          <Title> Quartz PE </Title>
+        </Header>
+        <Content style={styles.content}>
+          <Item style={styles.item} rounded>
+            <Input
+              placeholder="Username"
+              onChangeText={username => this.setState({ username })}
+            />
+          </Item>
+          <Item style={styles.item} rounded>
+            <Input
+              placeholder="Password"
+              onChangeText={password => this.setState({ password })}
+              secureTextEntry
+            />
+          </Item>
+
+          <Button style={styles.button} onPress={this._handleLogin} rounded>
+            <Text style={styles.text}>LOGIN</Text>
+          </Button>
+          <Button style={styles.button2} bordered>
+            <Text style={styles.text2}>REGISTER</Text>
+          </Button>
+        </Content>
+      </Container>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
-    header:{
-       alignItems: 'center',
-       backgroundColor: '#34495e'
-    },
-    content: {
-        flex: 1,
-        paddingVertical: '20%',
-        paddingHorizontal: '5%',
-    },
-    item:{
-        marginVertical: '2%',
-    },
-    button:{
-        width: '100%',
-        backgroundColor: '#34495ecc',
-    },
-    button2:{
-        width: '50%',
-        alignItems: 'center',
-        marginVertical: '2%',
-        borderRadius: 100,
-        borderColor: '#34495e',
-
-    },
-    text:{
-        color: '#ffffff',
-        width: '100%',
-        textAlign: 'center'
-    },
-    text2:{
-        color: '#34495e',
-        width: '100%',
-        textAlign: 'center'
-    },
-})
+  header: {
+    alignItems: "center",
+    backgroundColor: "#34495e"
+  },
+  content: {
+    flex: 1,
+    paddingVertical: "20%",
+    paddingHorizontal: "5%"
+  },
+  item: {
+    marginVertical: "2%"
+  },
+  button: {
+    width: "100%",
+    backgroundColor: "#34495ecc"
+  },
+  button2: {
+    width: "50%",
+    alignItems: "center",
+    marginVertical: "2%",
+    borderRadius: 100,
+    borderColor: "#34495e"
+  },
+  text: {
+    color: "#ffffff",
+    width: "100%",
+    textAlign: "center"
+  },
+  text2: {
+    color: "#34495e",
+    width: "100%",
+    textAlign: "center"
+  }
+});
